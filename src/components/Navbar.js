83756785d7cccd898ce5f3e21487e9bd4398f989
prince-sunsara <princesunasara977@gmail.com>
2023-08-14 00:24:43 +0530

    fevicon added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,12 +14,12 @@ const Navbar = () =>{
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/business">business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment">entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/health">health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science">science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports">sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology">technology</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
                         </ul>
                     </div>
                 </div>
